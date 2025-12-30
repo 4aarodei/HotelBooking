8790f2da6fa2881d2e71f084625c8a59ccb29362
Максим'як Данило <marakasizd@gmail.com>
2025-12-30 20:36:00 +0200

    # Додано спільний layout профілю та бокову навігацію. # Додано централізовану систему UX-повідомлень через TempData.

---
+++ ViewModels/UserProfileViewModels/UserProfileIndexModel.cs
@@ -0,0 +1,11 @@
+﻿using HotelBooking.Models.Identity;
+
+namespace HotelBooking.ViewModels.UserProfileViewModels;
+
+public class UserProfileIndexModel
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+
+}
