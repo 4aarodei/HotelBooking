d1c1560c4eb4fe8f18f4dae9b7bfc4edce5e03e9
Максим'як Данило <marakasizd@gmail.com>
2026-01-07 23:47:23 +0200

    Add booking request validation

--- HotelBooking.Web/ViewModels/CreateBookingRequest.cs
+++ HotelBooking.Web/ViewModels/CreateBookingRequest.cs
@@ -1,10 +1,25 @@
-﻿namespace HotelBooking.ViewModels
-{
-    public class CreateBookingRequest
-    {
-        public Guid RoomId { get; set; }
-        public DateTime CheckIn { get; set; }
-        public DateTime CheckOut { get; set; }
-    }
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace HotelBooking.Web.ViewModels
+{
+    public class CreateBookingRequest : IValidatableObject
+    {
+        [Required] public Guid RoomId { get; set; }
+
+        [Required][DataType(DataType.Date)] public DateTime CheckIn { get; set; }
+
+        [Required][DataType(DataType.Date)] public DateTime CheckOut { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (CheckOut <= CheckIn)
+            {
+                yield return new ValidationResult(
+                    "Check-out має бути пізніше Check-in",
+                    new[] { nameof(CheckOut) });
+            }
+        }
+
+    }
 }
